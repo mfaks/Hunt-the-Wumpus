c4c9236b7348eac6db8e4f4a8216688435122876
Muhammad Faks <muhammad.faks@gmail.com>
2022-12-27 09:56:07 -0800

    Update wumpus.h

---
+++ wumpus.h
@@ -0,0 +1,31 @@
+/******************************************************
+** Program: wumpus.h
+** Author: Muhammad Faks
+** Date: 11/19/2022
+** Description: Inhereted polymorphically from event class
+** Input: n/a
+** Output: Uses polymorphism to output the wumpus precept and enounter message
+******************************************************/
+
+#ifndef WUMPUS_H
+#define WUMPUS_H
+
+#include <iostream>
+#include <string>
+
+#include "event.h"
+
+using namespace std;
+
+class Wumpus : public Event { //inhereted from event class
+	protected:
+	
+	public:
+		Wumpus(); //default constructor
+		void precept(); //precept function
+		void encounter(); //encouter function
+		char event_letter(); //designating event letter function 
+
+};
+
+#endif
