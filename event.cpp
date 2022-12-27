e9e876ed2ebf94388c3ed4b3be161d429233907d
Muhammad Faks <muhammad.faks@gmail.com>
2022-12-27 09:52:24 -0800

    Update event.cpp

---
+++ event.cpp
@@ -0,0 +1,26 @@
+/******************************************************
+** Program: event.cpp
+** Author: Muhammad Faks 
+** Description: This is the abtrasct class that contains purely virutal functions
+** Input: Reads the precepts and enouncters of each event
+** Output: Uses polymorphism to output the correct precepts and enounters
+******************************************************/
+
+#include <iostream>
+#include <string>
+
+#include "event.h"
+
+using namespace std;
+
+/*********************************************************************
+** Function: Event()
+** Description: Default Constructor
+** Parameters: N/A
+** Pre-Conditions: N/A
+** Post-Conditions: New event object is created
+*********************************************************************/
+
+Event::Event(){
+	
+}
